e1dad924e23c2975ae05b20e3c36c35d9675edcc
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-08-10 17:17:24 +0100

    API now passes x-run-date header to services

--- src/middleware/calculate-ni.js
+++ src/middleware/calculate-ni.js
@@ -3,6 +3,6 @@ const nationalInsurance = require('../services/national-insurance');
 module.exports = (req, res) => {
   res.send({
     income: req.income,
-    ni: nationalInsurance()(req.income),
+    ni: nationalInsurance(req.get("x-run-date"))(req.income),
   });
 };
